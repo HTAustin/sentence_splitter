6e05bd940db078007d33eb409fd42378b8494084
HTAustin <socool.king@gmail.com>
2018-02-28 21:42:15 -0500

    remove tags

--- splitSentence.py
+++ splitSentence.py
@@ -10,6 +10,7 @@ tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
 folder = sys.argv[1]
 output = sys.argv[2]
+sentTotalCnt = 0
 for root, dirnames, filenames in os.walk(folder):
     for filename in filenames:        
         fp = open(os.path.join(root, filename))
@@ -17,7 +18,20 @@ for root, dirnames, filenames in os.walk(folder):
         data = unicode(data.strip(), errors='ignore')
         count = 0
         for sentence in tokenizer.tokenize(data):
-            writeF = open(output+'/'+filename+'.'+str(count), 'w')
+            sentence= sentence.strip()
+            # Remove tags <>
+            if sentence[0] == '<' and sentence[-1] == '>'
+                continue
+            diretory = str(sentTotalCnt/10000).zfill(5)
+            pathname = output+'/'+diretory+'/'+filename+'.'+str(count)
+            if not os.path.exists(os.path.dirname(pathname)):
+                try:
+                    os.makedirs(os.path.dirname(pathname))
+                except OSError as exc: # Guard against race condition
+                    if exc.errno != errno.EEXIST:
+                        raise
+            writeF = open(pathname, 'w')
             # sentence = unicode(sentence.strip(), errors='ignore')
             writeF.write(sentence.strip())             
-            count = count + 1
+            count += 1
+            sentTotalCnt += 1
